3a14aef216a25972b13def3b0e1dc5e5489dc25b
Carolista <cjones.renaissancewoman@gmail.com>
2025-06-04 23:58:28 -0400

    removed methods and added instructions with modifications to Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,30 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the first number: ");
         int a = scanner.nextInt();
         System.out.print("Enter the second number: ");
         int b = scanner.nextInt();
-        System.out.println("The sum is " + add(a, b));
-        System.out.println("The difference is " + subtract(a, b));
-        System.out.println("The product is " + multiply(a, b));
-        System.out.println("The quotient is " + divide(a, b));
+
+        // Use the static methods you wrote in the Calculator class
+        // to assign proper values to the four variables below
+        // (instead of 0)
+        int sum = 0;
+        int difference = 0;
+        int product = 0;
+        int quotient = 0;
+
+        System.out.println("The sum is " + sum);
+        System.out.println("The difference is " + difference);
+        System.out.println("The product is " + product);
+        System.out.println("The quotient is " + quotient);
     }
 
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static int subtract(int a, int b) {
-        return a - b;
-    }
-
-    public static int multiply(int a, int b) {
-        return a * b;
-    }
-
-    public static int divide(int a, int b) {
-        return a / b;
-    }
 }
