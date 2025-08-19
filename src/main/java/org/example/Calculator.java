9fccd9a3adfcaee8666fcb3ec9538b7330152d3d
wtwildnateis <wtwildnateis@gmail.com>
2025-08-19 16:50:09 -0500

    Updated assignment for LaunchCode!

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,10 +2,20 @@ package org.example;
 
 public class Calculator {
     // Add a static function called add that takes two int parameters and returns their sum (as an int)
-
-    // Add a static function called subtract that takes two int parameters and returns their difference (as an int)
-
-    // Add a static function called multiply that takes two int parameters and returns their product (as an int)
-
-    // Add a static function called divide that takes two int parameters and returns their quotient (as an int)
+public static int add(int a, int b) {
+    return a + b;
 }
+    // Add a static function called subtract that takes two int parameters and returns their difference (as an int)
+public static int subtract(int a, int b) {
+    return a - b;
+}
+    // Add a static function called multiply that takes two int parameters and returns their product (as an int)
+public static int multiply(int a, int b) {
+    return a * b;
+}
+    // Add a static function called divide that takes two int parameters and returns their quotient (as an int)
+public static int divide(int a, int b) {
+    return a / b;
+}
+}
+
--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -5,12 +5,30 @@ public class StringUtilities {
 
     // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
     // characters
+    public static boolean shortString(String input) {
+        return input.length() < 5;
+    }
 
     // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
+    public static char firstLetter(String input) {
+        return input.charAt(0);
+    }
 
     // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
-    // "asparagus" (lowercase only) replaced with 4 stars: "****"
+// "asparagus" (lowercase only) replaced with 4 stars: "****"
+    public static String censorAsparagus(String input) {
+        return input.replace("asparagus", "****");
+    }
 
     // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
-    // strings are the same length, return the first string.
+// strings are the same length, return the first string.
+    public static String bigger(String a, String b) {
+        if (a.length() > b.length()) {
+            return a;
+        } else if (b.length() > a.length()) {
+            return b;
+        } else {
+            return a;
+        }
+    }
 }
